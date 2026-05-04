8fc20b49a480b78df91c13e126729eb11f795f43
UsmanCyber66 <wes.kaneda@gmail.com>
2026-05-04 21:03:57 +0530

    sclient bug fixed

--- sclient.py
+++ sclient.py
@@ -15,13 +15,13 @@ app.add_typer(add, name="add")
 def user():
     username = inpute("Username for new user: ")
     password = getepass()
-    combohash = baseify(sha(username + password))
+    combohash = baseify(sha(username + password)).decode()
     print("User added successfully.")
     data= {
         "combohash": combohash
     }
     with open(baseify(sha(username)), "w") as f:
-        json.dump(data, f)
+        json.dump(data, f, indent=4)
 
 if __name__ == "__main__":
     app()
