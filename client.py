d8a07fdae5479810febee9a64e1f00a92361a351
UsmanCyber66 <wes.kaneda@gmail.com>
2026-05-12 00:08:01 +0530

    client.py updated

--- client.py
+++ client.py
@@ -5,8 +5,8 @@ async def main():
     uri = "ws://localhost:8765"
     async with websockets.connect(uri) as websocket:
         await websocket.send("login") 
-        nonce = await websocket.recv()
         username = baseify(sha(inpute("Username: ")))
+        nonce = await websocket.recv()
         auth_token = noncify(username, nonce)
         await websocket.send(auth_token)
         response = await websocket.recv()
